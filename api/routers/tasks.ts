ca67376c0b50ef8d05f378cb54dc661b1faa4836
Silalieva Begimay <begimayomurovna@gmail.com>
2025-04-22 14:46:04 +0600

    создан put запрос для редактирования своих задач

--- api/routers/tasks.ts
+++ api/routers/tasks.ts
@@ -32,4 +32,36 @@ tasksRouter.get("/", auth, async (req, res) => {
     res.send(tasks);
 });
 
+tasksRouter.put("/:id", auth, async (req, res, next) => {
+    const user = (req as RequestWithUser).user;
+    const id = req.params.id;
+    try {
+        const task = await Task.findOne({_id: id, user: user._id});
+
+        if (!task) {
+            res.status(404).send({error: 'You cannot edit just your task'});
+            return;
+        }
+
+        const taskUpdate = await Task.findOneAndUpdate(
+            {_id: id}, {
+                title: req.body.title,
+                description: req.body.description,
+                status: req.body.status
+            },
+            { new: true, runValidators: true}
+            );
+
+        console.log(taskUpdate);
+        res.send(taskUpdate);
+    } catch (error) {
+        if(error instanceof  Error.ValidationError) {
+            res.status(400).send(error);
+            return;
+        }
+
+        next(error);
+    }
+});
+
 export default tasksRouter;
