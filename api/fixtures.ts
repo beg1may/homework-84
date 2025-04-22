58656f5bb4b06d848e05cd38e5575687ed095e29
Silalieva Begimay <begimayomurovna@gmail.com>
2025-04-22 15:17:16 +0600

    Созданы фикстуры для задач

--- api/fixtures.ts
+++ api/fixtures.ts
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import config from "./config";
 import User from "./models/User";
+import Task from "./models/Task";
 
 const run = async () => {
     await mongoose.connect(config.db);
@@ -8,11 +9,12 @@ const run = async () => {
 
     try {
         await db.dropCollection('users');
+        await db.dropCollection('tasks');
     } catch (e) {
         console.error(e);
     }
 
-    await User.create(
+    const [userJane, userAlex] = await User.create(
         {
             username: 'Jane',
             password: '12345',
@@ -22,6 +24,21 @@ const run = async () => {
             username: 'Alex',
             password: '12345',
             token: crypto.randomUUID(),
+        }
+    );
+
+    await Task.create(
+        {
+            user: userJane,
+            title: 'Homework',
+            description: 'Do your homework',
+            status: 'complete',
+        },
+        {
+            user: userAlex,
+            title: 'Homework',
+            description: 'Do your homework',
+            status: 'complete',
         }
     )
 
